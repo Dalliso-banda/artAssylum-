ec2e3cd62057db392860f470935abca5ab7c7d7e
Frendolsk <112899523+Dalliso-banda@users.noreply.github.com>
2024-11-26 17:28:33 +0200

    Update app.js

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bp= require('body-parser');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const app= express();
 const port= 5000;
 console.log(mysql)
